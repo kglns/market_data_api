683fd9049c84fe1497ed34b07673678c8d45cfa6
Kaung Lin <kaung.lin@gmail.com>
2020-04-24 12:09:51 +0630

    Added tests for TimeSeries model

---
+++ test/models/time_series_test.rb
@@ -0,0 +1,23 @@
+require 'test_helper'
+
+class TimeSeriesTest < ActiveSupport::TestCase
+  test "should not create StockPrice without symbol" do
+    stock_price = StockPrice.new
+    assert_not stock_price.save
+  end
+
+  test "should not create TimeSeries without symbol" do
+    time_series = TimeSeries.new
+    assert_not time_series.save
+  end
+  
+  test "should not create TimeSeries without stock_price_id" do
+    time_series = TimeSeries.new(symbol: 'ABC')
+    assert_not time_series.save
+  end
+  
+  test "should create a TimeSeries given symbol and stock_price_id" do
+    time_series = TimeSeries.new(symbol: 'ABC', stock_price_id: StockPrice.last.id)
+    assert time_series.save
+  end
+end
